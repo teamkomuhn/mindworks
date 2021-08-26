b872df3ee9791d7af7dbd4d0913da458551406d5
adonis <andrealou.adao@gmail.com>
2021-08-26 19:23:05 +0100

    page adjustments
    move title and excerpts

--- page.php
+++ page.php
@@ -10,19 +10,23 @@
             <section class="single-mw-tools">
                 
                 <?php
+
+                    $post_label = get_post_meta( $post->ID, 'label_meta', 1);
                     $post_cover_text = get_post_meta( $post->ID, 'cover_meta', 1);
                     $post_style = 'style="background-image:url(' . get_the_post_thumbnail_url($post->ID) . ')"';
                 ?>
                     <header class="cover" <?php print $post_style; ?>>
                         <div class="cover-content">
-                            <h1><?php the_title(); ?></h1>
-                            <h2><?php print $post_cover_text; ?></h2>
+                            <h1><?php echo get_the_title($post->post_parent); ?></h1>
                             <span class="date"><?php the_date( 'F d, Y' ); ?></span>
                             <span class="readtime" id="time"></span> min read
                         </div>
                     </header>
 
                     <div class="content" id="content">
+                        <label><?php echo $post_label; ?></label>
+                        <h2 class="title"><?php the_title(); ?></h2>
+                        <h3 class="excerpt"><?php print $post_cover_text; ?></h3>
                         <?php the_content(); ?>
                     </div>
 
