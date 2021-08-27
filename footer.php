76df704a0e89bdce922aec399cdb3575df73c9cb
adonis <andrealou.adao@gmail.com>
2021-08-27 18:59:06 +0100

    header-footer-css
    header
    footer
    css - main and full
    mixins, variables
    fonts - icomoon
    favicon + logos

--- footer.php
+++ footer.php
@@ -1,14 +1,66 @@
+        </main>
+        
+        <footer class="main-footer">
+            <div class="two-columns">
+                <div class="column">
+                    <div class="identity">
+                        <a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>" alt="Logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/logo-white.svg" alt="<?php bloginfo('name'); ?>" />
+                        </a>
+                    </div>
+                    <h2>Giving changemakers the power to understand how the human mind works.</h2>
+                    <h3>Mindworks translates insights from cognitive and social sciences into practical tools for audience research, campaign strategy and engagement design.</h3>
+                </div>
+                <div class="column">
+                    <div class="subscription-form">
+                        <h4>Our new website is coming soon!</h4>
+                        <p>Join our mailing list to be the first to know when.</p>
+                        <div class="mailchimp-form">
+                            <!-- Begin Mailchimp Signup Form -->
+                            <div id="mc_embed_signup">
+                                <form action="https://mailchi.us20.list-manage.com/subscribe/post?u=ca3248c21dbaac156fd95fe92&amp;id=2314269c11" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                    <div id="mc_embed_signup_scroll">
+                                        <input type="text" value="" name="FNAME" class="name" id="mce-FNAME" placeholder="Name">
+                                        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="E-mail" required>
+                                        <!--<div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+                                            <div class="content__gdpr">
+                                                <label>Marketing Permissions</label>
+                                                <fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
+                                                    <label class="checkbox subfield" for="gdpr_38006"><input type="checkbox" id="gdpr_38006" name="gdpr[38006]" value="Y" class="av-checkbox "><span>Email</span></label>
+                                                    <label class="checkbox subfield" for="gdpr_38010"><input type="checkbox" id="gdpr_38010" name="gdpr[38010]" value="Y" class="av-checkbox "><span>Direct Mail</span></label>
+                                                    <label class="checkbox subfield" for="gdpr_38014"><input type="checkbox" id="gdpr_38014" name="gdpr[38014]" value="Y" class="av-checkbox "><span>Customized Online Advertising</span></label>
+                                                </fieldset>
+                                            </div>
+                                        </div>-->
+                                        <div id="mce-responses" class="clear">
+                                            <div class="response" id="mce-error-response" style="display:none"></div>
+                                            <div class="response" id="mce-success-response" style="display:none"></div>
+                                        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ca3248c21dbaac156fd95fe92_2314269c11" tabindex="-1" value=""></div>
+                                        <div class="clear"><input type="submit" value="Send" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                    </div>
+                                </form>
+                            </div>
+                            <!--End mc_embed_signup-->
+                        </div>
+                        <span class="icon-email">
+                            <b>Email</b><br>
+                            <a href="mailto:hello@mindworkslab.org" alt="hello@mindworkslab.org">hello@mindworkslab.org</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="so-me">
+                        <a href="https://www.linkedin.com/company/mindworks-lab/" class="icon icon-linkedin" alt="Mindworks Linkedin"></a>
+                        <a href="https://twitter.com/mindworkslab" class="icon icon-twitter" alt="Mindworks Twitter"></a>
+                    </div>
+                    <a href="https://docs.google.com/document/d/1SBKEhdD9D9nTdGMZbxMUaOvrSW1ZWH3LjeNQ-uGDrZg/edit?usp=sharing" alt="Mindworks Privacy Policy">Privacy Policy</a>
+                </div>
+            </div>
+        </footer>
+        
+    <?php wp_footer(); ?>
 
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/modernizr-3.11.2.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/plugins.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
-
-    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-    <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-    </script>
-    <script src="https://www.google-analytics.com/analytics.js" async></script>
-</body>
+    </body>
 
 </html>
--- header.php
+++ header.php
@@ -2,24 +2,64 @@
 <html class="no-js" lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Global site tag (gtag.js) - Google Analytics 
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-063HXFCJKX"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
 
-    <meta property="og:title" content="">
-    <meta property="og:type" content="">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
+		gtag('config', 'G-063HXFCJKX');
+	</script> -->
 
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="icon.png">
-    <!-- Place favicon.ico in the root directory -->
+	<meta charset="utf-8">
 
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/normalize.css">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/main.css">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/main.css">
-    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="<?php echo get_stylesheet_directory_uri(); ?>/css/full.css">
+	<?php wp_head(); ?>
 
-    <meta name="theme-color" content="#222222">
+	<link rel='shortcut icon' href='/img/favicon.ico' type='image/x-icon' />
+
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+	<meta name="author" content="Komuhn">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/main.css">
+	<link rel="stylesheet" media="screen and (min-width: 1025px)" href="<?php echo get_stylesheet_directory_uri(); ?>/css/full.css">
+
 </head>
+
+<body>
+	<!--[if IE]>
+			<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	<![endif]-->
+	<?php
+		if (is_home() ) {
+			$logoColor = "white";
+			$bgColor = "bgBlack";
+		} else {
+			$logoColor = "black";
+			$bgColor = "bgWhite";
+		}
+	?>
+	<header class="main-header <?php echo $bgColor; ?>">
+		<div class="identity">
+			<h1 class="logo">
+				<a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>" alt="Logo">
+					<span><?php bloginfo('name'); ?></span>
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo-<?php echo $logoColor; ?>.svg" alt="<?php bloginfo('name'); ?>" />
+				</a>
+			</h1>
+		</div>
+
+		<!--<nav class="main-nav" id="main-nav">
+			<?php /*
+				wp_nav_menu( array(
+					'menu'            => 'top-menu',
+					'container'       => ''
+				));
+			*/?>
+		-->
+		</nav>
+	</header>
+    <main>
