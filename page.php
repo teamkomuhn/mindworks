5aca9f800a198e80a02edbf25d825e994001d4d9
uxte <pedro.reis@uxte.com>
2021-08-29 18:11:43 +0100

    Build page template

--- page.php
+++ page.php
@@ -1,11 +1,7 @@
 <?php get_header(); ?>
 
-<body>
-
-    <main>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
     <?php endwhile; endif; ?>
-    </main>
 
 <?php get_footer(); ?>
