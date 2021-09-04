5e08aefe3df12db6df8c7f0b15d3dd00cc0ca3bb
adonis <andrealou.adao@gmail.com>
2021-09-04 23:22:35 +0100

    Made adjustments to page-scientific-insights + icomoon content ref

--- page-scientific-insights.php
+++ page-scientific-insights.php
@@ -8,17 +8,17 @@
             $post_cover_text = get_post_meta( $post->ID, 'cover_meta', 1);
             $post_style = 'style="background-image:url(' . get_the_post_thumbnail_url($post->ID) . ')"';
         ?>
-            <header class="cover" <?php print $post_style; ?>>
+            <!--<header class="cover" <?php //print $post_style; ?>>
                 <div class="cover-content">
-                    <h1><?php echo get_the_title($post->post_parent); ?></h1>
-                    <span class="date"><?php the_date( 'F d, Y' ); ?></span>
+                    <h1><?php// echo get_the_title($post->post_parent); ?></h1>
+                    <span class="date"><?php //the_date( 'F d, Y' ); ?></span>
                     <span class="readtime" id="time"></span> min read
                 </div>
-            </header>
+            </header>-->
 
             <div class="content" id="content">
                 <label><?php echo $post_label; ?></label>
-                <h2 class="title"><?php the_title(); ?></h2>
+                 <!--<h2 class="title"><?php the_title(); ?></h2>-->
                 <h3 class="excerpt"><?php print $post_cover_text; ?></h3>
                 <?php the_content(); ?>
             </div>
