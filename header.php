fc749547c26c351ffbebd16e830b89a4b14fe35a
adonis <andrealou.adao@gmail.com>
2021-09-06 21:45:38 +0100

    Made nav button change text on index page and homepage

--- header.php
+++ header.php
@@ -74,8 +74,20 @@
 				));
 			*/?>
 		</nav>-->
-
-		<a href="<?php echo home_url('thedisruptedmind/'); ?>" id="backToIndex" title="The Disrupted Mind"> Back to Index</a>
+		<?php
+			/*with pages ID - > if ( home_url() || home_url('thedisruptedmind/') ) {
+			if ( is_page( array(125,2,155) ) ) {
+				$buttonText = 'Back to Index';
+			} else {
+				$buttonText = 'Check our latest work';
+			}*/
+			if ( home_url() || home_url('thedisruptedmind/') ) {
+				$buttonText = 'Check our latest work';
+			} else {
+				$buttonText = 'Back to Index';
+			}
+		?>
+		<a href="<?php echo home_url('thedisruptedmind/'); ?>" id="backToIndex" title="The Disrupted Mind"><?php echo $buttonText; ?></a>
 
 	</header>
 
