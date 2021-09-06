af52bf1043d194d0e4b262ff912b1e0a5d24d4ff
adonis <andrealou.adao@gmail.com>
2021-09-06 19:29:22 +0100

    Removed id field from side note block

--- blocks/block-mw-side-note.php
+++ blocks/block-mw-side-note.php
@@ -1,4 +1,4 @@
-<aside class="block side-note" id="<?php block_field('id'); ?>">
+<aside class="block side-note">
     <span class="sup"></span>
     <?php block_field('content'); ?>
     <button type="button" class="close">Close</button>
