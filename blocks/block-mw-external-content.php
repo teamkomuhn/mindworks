751d0e0d1ce278c7359a08cced58955286ba37f1
adonis <andrealou.adao@gmail.com>
2021-09-06 22:00:31 +0100

    Clean code

--- blocks/block-mw-external-content.php
+++ blocks/block-mw-external-content.php
@@ -3,44 +3,7 @@
     $linkTitle1 = block_field('title1');
     $linkDescription1 = block_field('description1');
     $linkURL1 = block_field('url1');
-
-
-    /*$linkTitle2 = block_field('title2');
-    $linkDescription2 = block_field('description2');
-    $linkURL2 = block_field('url2');
-
-    $linkTitle3 = block_field('title3');
-    $linkDescription3 = block_field('description3');
-    $linkURL3 = block_field('url3');
-
-    $linkTitle4 = block_field('title4');
-    $linkDescription4 = block_field('description4');
-    $linkURL4 = block_field('url4');
-    
-    $linkTitle5 = block_field('title5');
-    $linkDescription5 = block_field('description5');
-    $linkURL5 = block_field('url5');
-    
-    $linkTitle6 = block_field('title6');
-    $linkDescription6 = block_field('description6');
-    $linkURL6 = block_field('url6');
-    
-    $linkTitle7 = block_field('title7');
-    $linkDescription7 = block_field('description7');
-    $linkURL7 = block_field('url7');
-    
-    $linkTitle8 = block_field('title8');
-    $linkDescription8 = block_field('description8');
-    $linkURL8 = block_field('url8');
-    
-    $linkTitle9 = block_field('title9');
-    $linkDescription9 = block_field('description9');
-    $linkURL9 = block_field('url9');
-    
-    $linkTitle10 = block_field('title10');
-    $linkDescription10 = block_field('description10');
-    $linkURL10 = block_field('url10');
-
+?>
 <section class="block external-content">
     <header>
         <h1>Section title - References? External content</h1>
