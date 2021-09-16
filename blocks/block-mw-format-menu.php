2ec52219634983dfdd46c1d05a37d94115c996de
uxte <pedro.reis@uxte.com>
2021-09-16 11:57:22 +0100

    Small adjustments to format-menu

--- blocks/block-mw-format-menu.php
+++ blocks/block-mw-format-menu.php
@@ -4,9 +4,9 @@
         <li class="download sub-menu">
             <a role="button">Download</a>
             <ul>
-                <li><a href="#" role="link" download>English</a></li>
-                <li><a href="#" role="link" download>Français</a></li>
-                <li><a href="#" role="link" download>Português</a></li>
+                <li><a href="https://testing.mindworkslab.org/wp-content/uploads/The-Disrupted-Mind-Part-2-The-crisis-timeline-Mindworks.pdf" download>English</a></li>
+                <li><a href="#" download>Français</a></li>
+                <li><a href="#" download>Português</a></li>
             </ul>
         </li>
         <li class="listen"><a href="#" role="link">Listen</a></li>
