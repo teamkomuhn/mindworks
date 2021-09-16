f74d792d622726255f19efc646096e5161e49038
uxte <pedro.reis@uxte.com>
2021-09-16 14:59:42 +0100

    Make new footer

--- footer.php
+++ footer.php
@@ -5,63 +5,67 @@
         </aside>
 
         <footer class="main-footer">
-            <div class="two-columns">
-                <div class="column">
-                    <div class="identity">
-                        <a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>" alt="Logo">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/logo-white.svg" alt="<?php bloginfo('name'); ?>" />
-                        </a>
-                    </div>
-                    <h2>Giving changemakers the power to understand how the human mind works.</h2>
-                    <h3>Mindworks translates insights from cognitive and social sciences into practical tools for audience research, campaign strategy and engagement design.</h3>
-                </div>
-                <div class="column">
-                    <div class="subscription-form">
-                        <h4>Our new website is coming soon!</h4>
-                        <p>Join our mailing list to be the first to know when.</p>
-                        <div class="mailchimp-form">
-                            <!-- Begin Mailchimp Signup Form -->
-                            <div id="mc_embed_signup">
-                                <form action="https://mailchi.us20.list-manage.com/subscribe/post?u=ca3248c21dbaac156fd95fe92&amp;id=2314269c11" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                                    <div id="mc_embed_signup_scroll">
-                                        <input type="text" value="" name="FNAME" class="name" id="mce-FNAME" placeholder="Name">
-                                        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="E-mail" required>
-                                        <!--<div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
-                                            <div class="content__gdpr">
-                                                <label>Marketing Permissions</label>
-                                                <fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
-                                                    <label class="checkbox subfield" for="gdpr_38006"><input type="checkbox" id="gdpr_38006" name="gdpr[38006]" value="Y" class="av-checkbox "><span>Email</span></label>
-                                                    <label class="checkbox subfield" for="gdpr_38010"><input type="checkbox" id="gdpr_38010" name="gdpr[38010]" value="Y" class="av-checkbox "><span>Direct Mail</span></label>
-                                                    <label class="checkbox subfield" for="gdpr_38014"><input type="checkbox" id="gdpr_38014" name="gdpr[38014]" value="Y" class="av-checkbox "><span>Customized Online Advertising</span></label>
-                                                </fieldset>
-                                            </div>
-                                        </div>-->
-                                        <div id="mce-responses" class="clear">
-                                            <div class="response" id="mce-error-response" style="display:none"></div>
-                                            <div class="response" id="mce-success-response" style="display:none"></div>
-                                        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ca3248c21dbaac156fd95fe92_2314269c11" tabindex="-1" value=""></div>
-                                        <div class="clear"><input type="submit" value="Send" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                                    </div>
-                                </form>
-                            </div>
-                            <!--End mc_embed_signup-->
-                        </div>
-                        <span class="icon-email">
-                            <b>Email</b><br>
-                            <a href="mailto:hello@mindworkslab.org" alt="hello@mindworkslab.org">hello@mindworkslab.org</a>
-                        </span>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="so-me">
-                        <a href="https://www.linkedin.com/company/mindworks-lab/" class="icon icon-linkedin" alt="Mindworks Linkedin"></a>
-                        <a href="https://twitter.com/mindworkslab" class="icon icon-twitter" alt="Mindworks Twitter"></a>
-                    </div>
-                    <a class="text-small" href="https://docs.google.com/document/d/1SBKEhdD9D9nTdGMZbxMUaOvrSW1ZWH3LjeNQ-uGDrZg/edit?usp=sharing" alt="Mindworks Privacy Policy">Privacy Policy</a>
-                </div>
+
+            <div class="identity">
+                <h1 class="logo">
+                    <a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>" alt="Logo">
+                        <span>Mindworks</span>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/logo-white.svg" alt="<?php bloginfo('name'); ?>" />
+                    </a>
+                </h1>
+                <h2>Giving changemakers the power to understand how the human mind works.</h2>
+                <h3>Mindworks translates insights from cognitive and social sciences into practical tools for audience research, campaign strategy and engagement design.</h3>
             </div>
+
+            <div class="form mailchimp">
+                <h4>Our new website is coming soon!</h4>
+                <p>Join our mailing list to be the first to know when.</p>
+
+                <!-- Begin Mailchimp Signup Form -->
+                <div id="mc_embed_signup">
+                    <form action="https://mailchi.us20.list-manage.com/subscribe/post?u=ca3248c21dbaac156fd95fe92&amp;id=2314269c11" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+                            <input type="text" value="" name="FNAME" class="name" id="mce-FNAME" placeholder="Name">
+                            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="E-mail" required>
+                            <!--<div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+                                <div class="content__gdpr">
+                                    <label>Marketing Permissions</label>
+                                    <fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
+                                        <label class="checkbox subfield" for="gdpr_38006"><input type="checkbox" id="gdpr_38006" name="gdpr[38006]" value="Y" class="av-checkbox "><span>Email</span></label>
+                                        <label class="checkbox subfield" for="gdpr_38010"><input type="checkbox" id="gdpr_38010" name="gdpr[38010]" value="Y" class="av-checkbox "><span>Direct Mail</span></label>
+                                        <label class="checkbox subfield" for="gdpr_38014"><input type="checkbox" id="gdpr_38014" name="gdpr[38014]" value="Y" class="av-checkbox "><span>Customized Online Advertising</span></label>
+                                    </fieldset>
+                                </div>
+                            </div>-->
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ca3248c21dbaac156fd95fe92_2314269c11" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Send" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                        </div>
+                    </form>
+                </div>
+                <!--End mc_embed_signup-->
+
+                <span class="icon-email">
+                    <b>Email</b><br>
+                    <a href="mailto:hello@mindworkslab.org" alt="hello@mindworkslab.org">hello@mindworkslab.org</a>
+                </span>
+
+            </div>
+
+            <div class="so-me">
+                <a href="https://www.linkedin.com/company/mindworks-lab/" class="icon icon-linkedin" alt="Mindworks Linkedin"></a>
+                <a href="https://twitter.com/mindworkslab" class="icon icon-twitter" alt="Mindworks Twitter"></a>
+            </div>
+            <a class="text-small" href="https://docs.google.com/document/d/1SBKEhdD9D9nTdGMZbxMUaOvrSW1ZWH3LjeNQ-uGDrZg/edit?usp=sharing" alt="Mindworks Privacy Policy">Privacy Policy</a>
+
+
+
         </footer>
+
+
 
     <?php wp_footer(); ?>
 
