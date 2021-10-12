2b746f32ed28dbefd1347a553a818b6aa7da5209
adonis <andrealou.adao@gmail.com>
2021-10-12 10:21:50 +0100

    Updated pdf links

--- blocks/block-mw-format-menu.php
+++ blocks/block-mw-format-menu.php
@@ -4,9 +4,9 @@
         <li class="download sub-menu">
             <a role="button">Download</a>
             <ul>
-                <li><a href="http://mindworkslab.org/wp/content/uploads/The-Disrupted-Mind-Part-2-The-crisis-timeline-Mindworks.pdf" download>English</a></li>
-                <li><a href="http://mindworkslab.org/wp/content/uploads/The-Disrupted-Mind-Part-2_-The-crisis-timeline-Mindworks_ES.pdf" download>Español</a></li>
-                <li><a href="http://mindworkslab.org/wp/content/uploads/The-Disrupted-Mind-Part-2_-The-crisis-timeline-Mindworks_PT.pdf" download>Português</a></li>
+                <li><a href="http://mindworkslab.org/wp/wp-content/uploads/The-Disrupted-Mind-Part-2-The-crisis-timeline-Mindworks.pdf" download>English</a></li>
+                <li><a href="http://mindworkslab.org/wp/wp-content/uploads/The-Disrupted-Mind-Part-2_-The-crisis-timeline-Mindworks_ES.pdf" download>Español</a></li>
+                <li><a href="http://mindworkslab.org/wp/wp-content/uploads/The-Disrupted-Mind-Part-2_-The-crisis-timeline-Mindworks_PT.pdf" download>Português</a></li>
             </ul>
         </li>
         <li class="listen"><a href="https://soundcloud.com/mindworkslab/the-disrupted-mind-part-2-the-crisis-timeline" role="link">Listen</a></li>
