a172197481975a9a9c447c0f7113269f77259f71
adonis <andrealou.adao@gmail.com>
2021-10-26 19:32:27 +0100

    Call cards on the handbook template by category

--- template-handbooks.php
+++ template-handbooks.php
@@ -4,8 +4,11 @@
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <?php the_excerpt(); ?>
-        <?php the_content(); ?>
+        <?php
+            the_title();
+            the_excerpt();
+            the_content();
+        ?>
 
         <?php
             $postcat = get_the_category( $post->ID );
@@ -20,9 +23,34 @@
                             <h1><?php echo $cat->name; ?></h1>
                             <p><?php echo $cat->description; ?></p>
                             <figure>
-                                <img src="<?php echo wp_get_attachment_image($imageID, 'medium'); ?>"/>
+                                <?php echo wp_get_attachment_image($imageID, 'medium'); ?>
                             </figure>
                         </header> 
+
+                        <?php
+			                // WP_Query arguments
+                            $args = array(
+                                'post_type'      => 'cards',
+                                'posts_per_page' => -1,
+                                'cat'            =>  $cat->cat_ID,
+                            );
+
+                            // The Query
+                            $cards = new WP_Query( $args );
+                
+                            // The Loop
+                            if ( $cards->have_posts() ) :
+                        ?>
+                                <ul>
+                                    <?php
+                                        while ( $cards->have_posts() ) : $cards->the_post(); 
+                                    ?>
+                                                <li><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></li>
+                                    <?php
+                                        endwhile; 
+                                    ?>
+                                </ul>
+                        <?php endif; ?>
                         
                     </article>
 
