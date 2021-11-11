1ba15d0af4bfa98ba3113caeb8f9a0a2cadbba3a
adonis <andrealou.adao@gmail.com>
2021-11-11 12:48:57 +0000

    Updated page-handbook structure, get content
    + added function limit_text

--- functions.php
+++ functions.php
@@ -422,4 +422,15 @@
         }
     }
 
+    //TRIM TEXT - limit words
+
+    function limit_text($text, $limit) {
+        if (str_word_count($text, 0) > $limit) {
+            $words = str_word_count($text, 2);
+            $pos   = array_keys($words);
+            $text  = substr($text, 0, $pos[$limit]) . '...';
+        }
+        return $text;
+    }
+    
 ?>
--- page-handbook.php
+++ page-handbook.php
@@ -2,55 +2,72 @@
 <?php get_header(); ?>
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php //the_content(); ?>
-    <?php endwhile; endif; ?>
-
-    <header>
-        <h1>The crisis handbook</h1>
-        <h2>A toolkit for changemakers</h2>
-    </header>
-
-    <section class="category">
-        <header>
-            <h2>The early phases of a crisis</h2>
-        </header>
-
-        <article class="card">
+        
             <header>
-                <h3>Sense, Orient, Decide</h3>
-                <figure>
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/icon-sense.svg" alt="">
-                </figure>
-                <a class="button open" href="<?php echo esc_url( get_permalink( get_page_by_title( 'Card 1' ) ) ); ?>">Open card</a>
+                <h1><?php the_title(); ?></h1>
+                <h2><?php echo get_the_excerpt(); ?></h2>
             </header>
-            <p>Use your team’s capacity to observe the crisis and speak to different stakeholders as early as possible...</p>
-        </article>
-
-        <article class="card">
-            <header>
-                <h3>Normalise new experiences and thoughts</h3>
-                <figure>
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/icon-sense.svg" alt="">
-                </figure>
-                <button class="open" type="button" name="button">Open card</button>
-
-            </header>
-            <p>Use your team’s capacity to observe the crisis and speak to different stakeholders as early as possible...</p>
-        </article>
-
-        <article class="card">
-            <header>
-                <h3>Card 3</h3>
-                <figure>
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/icon-sense.svg" alt="">
-                </figure>
-                <button class="open" type="button" name="button">Open card</button>
-
-            </header>
-            <p>Use your team’s capacity to observe the crisis and speak to different stakeholders as early as possible...</p>
-        </article>
 
 
-    </section>
+            <?php
+                // Get handbook sections, organized by categories
+                // Get handbook cards, organized by categories
+
+                $postcat = get_the_category( $post->ID );
+
+                foreach( $postcat as $cat ) {
+                    // WP_Query arguments
+                    $args = array(
+                        'post_type'      => 'page',
+                        'posts_per_page' => -1,
+                        'post_parent'    => $post->ID,
+                        'cat'            => $cat->cat_ID,
+                    );
+
+                    // The Query
+                    $cards = new WP_Query( $args );
+
+                    if ($cat->parent != 0 && $cards->have_posts() ) {
+                        $imageID = get_term_meta ( $cat->cat_ID, 'category-image-id', true );
+            ?>
+
+                        <section class="category">
+                            <header>
+                                <h2><?php echo $cat->name; ?></h2>
+                                <p><?php echo $cat->description; ?></p>
+                                <?php /* CAT IMAGE
+                                    if($imageID != ''){ ?>
+                                    <figure>
+                                        <?php echo wp_get_attachment_image($imageID, 'medium'); ?>
+                                    </figure>
+                                <?php }*/ ?>
+                            </header> 
+
+                            <?php
+                    
+                                // The Loop
+                                while ( $cards->have_posts() ) : $cards->the_post();
+                            ?>
+                                    <article class="card">
+                                        <header>
+                                            <h3><?php echo get_the_title(); ?></h3>
+                                            <figure>
+                                                <img src="<?php echo get_template_directory_uri(); ?>/img/icon-sense.svg" alt="">
+                                            </figure>
+                                            <a class="button open" href="<?php echo esc_url( get_permalink( get_the_ID() ) ); ?>">Open card</a>
+                                        </header>
+                                        <p><?php echo limit_text(get_the_excerpt(), 20) ?></p>
+                                    </article>
+
+                            <?php endwhile; wp_reset_postdata();  ?>
+                            
+                        </section>
+
+            <?php
+                    }
+                } 
+            ?>
+
+        <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
