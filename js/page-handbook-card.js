93df0d82fb60b6953adfc876ee9a8b5f76da7e2a
MR. INFINITY <ayo.v.reis@gmail.com>
2021-11-16 14:42:10 +0000

    Clean'd up version

--- js/page-handbook-card.js
+++ js/page-handbook-card.js
@@ -23,18 +23,8 @@
 
     setTabs();
 
-    // TODO: Make a page able to have more than one expandable content section
-    // TODO: Break scrollTo() if user scrolls manualy
-    // TODO: If one of the top panels is open, calculate of the onScroll() also the height of that closing
 
-    const scrollTo = ( element, duration = 0, offset = 0 ) => {
-        
-        $( [document.documentElement, document.body] ).animate({
-            scrollTop: window.scrollY + element.getBoundingClientRect().top - offset
-        }, { duration, easing: `linear` })
-        
-    }
-    
+    // TODO: Make a page able to have more than one expandable content section
 
     const containers = document.querySelectorAll(`.container-expandable`)
     const allExpandables = []
@@ -43,6 +33,8 @@
     
 
     for (const [index, container] of containers.entries()) {
+
+        const DURATION = 250
 
         const button = container.querySelector(`.button-expandable`)
         const expandable = container.querySelector(`.expandable`)
@@ -53,28 +45,15 @@
         expandable.style.height = `0px`
 
         button.addEventListener(`click`, event => {
-
-            const DURATION = 250
             const OFFSET = container.getBoundingClientRect().top
 
-            let isOpen = expandable.style.height !== `0px`
+            const isOpen = expandable.style.height !== `0px`
 
             if (isOpen) {
                 expandable.style.height = `0px`
-
-                // openHeight -= heights[index]
-                // isOpen = expandable.style.height !== `0px`
             } else {
                 expandable.style.height = `${heights[index]}px`
-
-                // openHeight += heights[index]
-                // isOpen = expandable.style.height !== `0px`
             }
-
-            // const otherHeight = openHeight - (isOpen ? heights[index] : 0)
-            // console.log(otherHeight)
-
-            scrollTo(container, DURATION, OFFSET)
         })
 
     }
