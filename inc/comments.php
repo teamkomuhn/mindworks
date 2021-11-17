28e7fc8674b43ac8138dd087fd0637d55f9c5c76
adonis <andrealou.adao@gmail.com>
2021-11-17 09:30:25 +0000

    Updated comments template and added vars for custom copy

--- inc/comments.php
+++ inc/comments.php
@@ -1,10 +1,7 @@
-<section class="comments">
-    <header>
-        <h3>Leave us a message</h3>
-        <p>If you’d be interested in collaborating, learning more or sharing your feedback and comments please use the form below.</p>
-    </header>
-    
 <?php
+
+    global $comment_field_placeholder, $submit_button_text;
+
     // START WP COMMENTS TEMPLATE
 
     //Comment Field Order
@@ -33,38 +30,10 @@
     //Remove cancel reply link
     add_filter( 'cancel_comment_reply_link', '__return_false' );
 
-    //Add select field to comments form for subjects
-    // add_filter( 'comment_form_field_comment', function( $field ) {
-    //
-    //     $subjects = ['I’d like to keep updated about Mindworks’ work',
-    //                 'I’d like to share some feedback on this paper or on The Disrupted Mind',
-    //                 'I’d love to collaborate',
-    //                 'Other'];
-    //
-    //     $select = '<p><label for="subject_select">Subject:</label>
-    //     <select name="subject_select" id="subjectSelect">
-    //     <option value="">Select a subject</option>';
-    //
-    //     foreach ( $subjects as $key => $subject )
-    //         $select .= sprintf(
-    //             '<option value="%1$s" %2$s>%3$s</option>',
-    //             esc_attr( $key ),
-    //             ( in_array( $key, $subject) ? 'selected' : '' ), //this is not working properly yet
-    //             esc_html( $subject )
-    //         );
-    //
-    //     $select .= '</select></p>';
-    //
-    //     return $select . $field;
-    // });
-
-	$args = array(
+	$commentsArgs = array(
         'title_reply'               => '',
         'title_reply_before'        => '',
         'title_reply_after'         => '',
-        /* translators: %s: Author of the comment being replied to. */
-        //'title_reply_to'          => __( 'Leave a Reply to %s' ),
-        //'cancel_reply_link'       => __( 'Cancel reply' ),
         'cancel_reply_before'       => '',
         'cancel_reply_after'        => '',
         'comment_notes_before'      => '',
@@ -78,7 +47,7 @@
                         'cookies'   => '',
 
 		) ),
-		'comment_field'             => '<textarea id="comment" name="comment" aria-required="true" placeholder="Write here your message or feedback"></textarea>',
+		'comment_field'             => '<textarea id="comment" name="comment" aria-required="true" placeholder="'.$comment_field_placeholder.'"></textarea>',
         // 'comment_notes_after'    => '<p class="comment-notes">Thanks for leaving us your message/feedback! We\'ll get back to you soon.</p>',
         //'id_form'                 => 'commentform',
         //'id_submit'               => 'submit',
@@ -88,12 +57,9 @@
         //'name_submit'             => 'submit',
 
         'submit_button'             => '<input name="%1$s" type="submit" id="%2$s" class="%3$s button" value="%4$s" />',
-        'label_submit'              => 'Send',
-        //'submit_field'            => '<p class="form-submit">%1$s %2$s</p>',
-        //'format'                  => 'xhtml',
+        'label_submit'              => $submit_button_text
 	);
-	comment_form( $args );
+
+	comment_form( $commentsArgs );
 
 ?>
-
-</section>
