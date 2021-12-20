ada0e349dd31f59afd5a55ae855d42b08943068f
Pedro Reis <pedro@komuhn.co>
2021-12-20 00:42:32 +0000

    Design home page

--- header.php
+++ header.php
@@ -32,31 +32,21 @@
 			<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
 	<![endif]-->
 	<?php
+	$old_page_TDMindex = get_page_by_path('thedisruptedmind');
+	$old_page_TDM1 = get_page_by_path('thedisruptedmind/scientific-insights/');
 
-		$old_page_TDMindex = get_page_by_path('thedisruptedmind');
-		$old_page_TDM1 = get_page_by_path('thedisruptedmind/scientific-insights/');
-
-		if ( is_page( $old_page_TDMindex->ID ) || is_page( $old_page_TDM1->ID ) ) {
-			$maxWidth = "class='max-width-pad margin-auto'";
-		} else {
-			$maxWidth = "class='max-width margin-auto'";
-		}
-
+	if ( is_page( $old_page_TDMindex->ID ) || is_page( $old_page_TDM1->ID ) ) {
+		$maxWidth = "class='max-width-pad margin-auto'";
+	} else if ( !is_home() ) {
+		$maxWidth = "class='max-width margin-auto'";
+	}
 	?>
 
 	<header class="main-header">
-
-		<?php if ( is_home() ) : ?>
-			<h1 class="logo">
-				<span><?php bloginfo('name') ?></span>
-				<img src="<?php echo get_template_directory_uri(); ?>/img/logo-black.svg" alt="<?php bloginfo('name'); ?>" />
-			</h1>
-		<?php else : ?>
-			<a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo('name') ?>">
-				<span><?php bloginfo('name') ?></span>
-				<img src="<?php echo get_template_directory_uri(); ?>/img/logo-black.svg" alt="<?php bloginfo('name'); ?>" />
-			</a>
-		<?php endif; ?>
+		<a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo('name') ?>">
+			<span><?php bloginfo('name') ?></span>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/logo-black.svg" alt="<?php bloginfo('name'); ?>" />
+		</a>
 
 		<button class="button nav open" type="button"><span>Open</span></button>
 		<nav class="main-nav">
@@ -91,7 +81,6 @@
 			<button class="button nav close" type="button"><span>Close</span></button>
 		</nav>
 
-
 	</header>
 
-    <main <?php echo $maxWidth; ?> id="main">
+	<main <?php echo $maxWidth; ?> id="main">
--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php get_header(); ?>
 
+<section class="about">
+	<header>
+	<div class="container">
+		<h1 class="logo">
+			<span><?php bloginfo('name') ?></span>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/logo-black.svg" alt="<?php bloginfo('name'); ?>" />
+		</h1>
+		<h2><span>Giving changemakers</span>
+			<span>the power to understand </span>
+			<span>how the human mind works.</span>
+		</h2>
+		<h3>Mindworks is an innovation lab currently based in Greenpeace East Asia that researches cognitive and social sciences and translates insights into practical tools built for changemakers. We focus on audience research, campaign strategy and engagement design.</h3>
+	</div>
+	</header>
+</section>
+
 <section class="latest-work">
 	<header>
 		<h1>Check our <strong>latest work</strong></h1>
@@ -17,6 +33,16 @@
 		<a class="button" href="<?php echo home_url( '/thedisruptedmind/' ); ?>">Learn more -></a>
 	</div>
 
+</section>
+
+<aside class="cta">
+	<p>Mindworks co-creates with <mark>organisations</mark>, <mark>teams</mark> and <mark>changemakers</mark> working to shift the hearts and minds of people in order to create mindset, power or structural change in complex social and environmental issues such as the climate crisis, polarisation, social unrest or crises.</p>
+	<a class="button go-to" href="#contacts">Talk with us</a>
+</aside>
+
+<section class="team">
+	<h2>Our mission</h2>
+	<p>We support changemakers working with communities, societies and decision makers to catalyse cultural change by shifting people’s hearts and minds. We leverage our team’s expertise and passion for the human mind to help changemakers develop innovative strategies, tactics and tools to understand and engage their audience in order to change the way they feel, think and act. </p>
 </section>
 
 <?php get_footer(); ?>
